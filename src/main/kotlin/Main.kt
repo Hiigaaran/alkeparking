880ef30e1e1365e438368ac55138a2fd6025b348
Hiigaaran <simo.moralesg@gmail.com>
2022-05-23 17:26:52 -0400

    se completa hasta el ejercicio 9

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,10 +14,12 @@ fun main() {
      *
      * Ejercicio 2:
      */
+    val calendarAdv: Calendar = Calendar.getInstance()
+    calendarAdv.set(2022, 5,23, 13, 0, 0)
     val car = Vehicle(plateGenerator(), VehicleType.CAR, Calendar.getInstance(), "DISCOUNT")
     val moto = Vehicle(plateGenerator(), VehicleType.BIKE, Calendar.getInstance(), null)
     val minibus = Vehicle(plateGenerator(), VehicleType.MINIBUS, Calendar.getInstance(), null)
-    val bus = Vehicle(plateGenerator(), VehicleType.BUS, Calendar.getInstance(), "DISCOUNT")
+    val bus = Vehicle(plateGenerator(), VehicleType.BUS, calendarAdv, "DISCOUNT")
     val car2 = Vehicle(plateGenerator(), VehicleType.CAR, Calendar.getInstance(), null)
     val car1 = Vehicle(plateGenerator(), VehicleType.CAR, Calendar.getInstance(), "DISCOUNT")
     val moto1 = Vehicle(plateGenerator(), VehicleType.BIKE, Calendar.getInstance(), null)
@@ -35,18 +37,10 @@ fun main() {
     val bus3 = Vehicle(plateGenerator(), VehicleType.BUS, Calendar.getInstance(), "DISCOUNT")
     val car6 = Vehicle(plateGenerator(), VehicleType.CAR, Calendar.getInstance(), null)
 
-    //val parking = Parking(mutableSetOf(car, moto, minibus, bus))
     val parking = Parking(mutableSetOf())
     val arrayOfVehicles = arrayOf(car, moto, minibus, bus, car2, car1, car3, car4, car5, car6, car7, bus1
     , bus2, bus3, moto1, moto2, moto3, minibus1, minibus2, minibus3, bus1, bus2, bus3)
 
-    //println(parking.vehicles.contains(car))
-    //println(parking.vehicles.contains(moto))
-    //println(parking.vehicles.contains(minibus))
-    //println(parking.vehicles.contains(bus))
-
-    //val isCar2Parked = parking.vehicles.add(car2)
-    //println(isCar2Parked)
     /**
      * Ejercicio 6.3: se recorre el arreglo de vehiculos creado para invocar la funcion addVehicle
      * para ingresar vehiculos en el parking, pasando por la validacion de plazas disponibles.
@@ -55,6 +49,12 @@ fun main() {
         if (parking.addVehicle(vehicle)) println("Welcome to AlkeParking!")
         else println("Sorry, the check-in has failed")
     }
+
+    val parkingSpace = ParkingSpace(car, parking)
+
+    parkingSpace.checkOutVehicle("bus.plate")
+
+
 
 }
 
--- src/main/kotlin/domain/ParkingSpace.kt
+++ src/main/kotlin/domain/ParkingSpace.kt
@@ -2,7 +2,7 @@ package src.main.kotlin.domain
 
 import java.util.Calendar
 
-data class ParkingSpace (var vehicle : Vehicle) {
+data class ParkingSpace (var vehicle : Vehicle, val parking: Parking) {
     /**
      * Ejercicio 4.1: Se agrega propiedad parkedTime en ParkingSpace para calcular el tiempo total de
      * estadia del vehiculo en el parking.
@@ -10,4 +10,30 @@ data class ParkingSpace (var vehicle : Vehicle) {
     val MINUTES_IN_MILISECONDS = 60000
     val parkedTime: Long
         get() = (Calendar.getInstance().timeInMillis - vehicle.checkInTime.timeInMillis) / MINUTES_IN_MILISECONDS
+
+    /**
+     * Ejercicio 7.1, 7.2 y 7.3
+     */
+    fun checkOutVehicle(plate: String): Unit {
+        var foundVehicle: Vehicle? = parking.vehicles.find { it.hashCode() == plate.hashCode() }
+        foundVehicle?.let { foundVehicle.discountCard?.let { onSuccess(calculateFee(foundVehicle.type, this.parkedTime, true)) } ?:
+        run { onSuccess(calculateFee(foundVehicle.type, this.parkedTime, false))} } ?: run { onError() }
+    }
+
+    fun onSuccess(fee: Int): Unit {
+        println("Your fee is $fee . Come back soon.")
+    }
+
+    fun onError(): Unit {
+        println("Se ejecuto onError")
+    }
+
+    fun calculateFee(type: VehicleType, parkedTime: Long, hasDiscountCard: Boolean): Int {
+        var subFee: Long = 0L
+        when{
+            parkedTime <= 120 -> subFee = type.fee.toLong()
+            parkedTime > 120 -> subFee = type.fee + ((parkedTime - 120)/15) * 5
+        }
+        if(hasDiscountCard) return (subFee * 0.85).toInt() else return subFee.toInt()
+    }
 }
